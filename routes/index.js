cdd97aab7b0a46f551095febf3d0afff17cad14f
Dacotah <deelcoding@outlook.com>
2025-01-30 19:27:43 -0500

    Project 13 Tests

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,15 @@ const clothingRouter = require("./clothingItems");
 const { login, createUser } = require("../controllers/users");
 const auth = require("../middleware/auth");
 
+
+router.post("/signin", login);
+router.post("/signup", createUser);
+
 router.use(auth);
 
 router.use("/users", userRouter);
 router.use("/items", clothingRouter);
-router.post("/signin", login);
-router.post("/signup", createUser);
+
 
 // Handling non-existent resources
 router.use((req, res) =>
