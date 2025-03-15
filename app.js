5b46c4ba1658fbda9076f1e30ed10080faef51c3
Dacotah <deelcoding@outlook.com>
2025-03-14 20:47:14 -0400

    Project 14

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 // Routes
-app.use("/api", routes);
+app.use("/", routes);
 
 // Connect to MongoDB server
 mongoose.set("strictQuery", false);
